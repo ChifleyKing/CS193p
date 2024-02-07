ca2028d159560c9d26c7e6bcb45341c576a1edf8
Aaron LOI <opboddy@gmail.com>
2024-02-07 22:46:18 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by HIN SENG LOI on 07/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
